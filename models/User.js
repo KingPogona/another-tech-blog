517078601d91fae943ce7dd79ac1722df6b35c47
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2020-10-29 23:20:29 -0600

    add initial user model and api routes

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const bcrypt = require('bcrypt');
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    // compare ecrypted password to entered password on login
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: { len: [6] }
+        }
+    },
+    {
+        // bcrypt hooks used to encrypt passwords when created or updated
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+
+// create associations
+
+// (associations will go here)
+
+module.exports = { User };
