200f1a5e9e8889a826cf2b8ce357285038cb0c49
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2020-11-01 00:15:37 -0600

    add initial html and css design for home, dashboard, login, and  sign up

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+const exphbs = require('express-handlebars');
+
+// set up app and port
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// set up template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
