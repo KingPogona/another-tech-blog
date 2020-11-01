892cd669f4c71988c8ba5e4da8c1ba8f49c27f1c
Asher Clawson <31211822+KingPogona@users.noreply.github.com>
2020-11-01 11:20:52 -0700

    Add express-session and connect-session-sequelize. update templates, public javascript, and home and user routes to make sessions work including the login/logout process.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,51 +7,62 @@ const { Post, User, Comment } = require('../models');
 router.get('/', (req, res) => {
     console.log(req.session);
 
-    // Post.findAll({
-    //     attributes: [
-    //         'id',
-    //         'post_url',
-    //         'title',
-    //         'created_at',
-    //         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    //     ],
-    //     include: [
-    //         {
-    //             model: Comment,
-    //             attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-    //             include: {
-    //                 model: User,
-    //                 attributes: ['username']
-    //             }
-    //         },
-    //         {
-    //             model: User,
-    //             attributes: ['username']
-    //         }
-    //     ]
-    // })
-    //     .then(dbPostData => {
-    //         const posts = dbPostData.map(post => post.get({ plain: true }));
-    //         // pass a single post object into the homepage template
-    //         res.render('homepage', {
-    //             posts,
-    //             loggedIn: req.session.loggedIn
-    //         });
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //         res.status(500).json(err);
-    //     });
-    res.render('homepage');
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_content', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+            
+        ]
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            // pass a single post object into the homepage template
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/login', (req, res) => {
     console.log(req.session);
+
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
     res.render('login');
 });
 
 router.get('/sign_up', (req, res) => {
     console.log(req.session);
+
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
     res.render('sign_up');
 });
 
--- server.js
+++ server.js
@@ -3,11 +3,24 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const exphbs = require('express-handlebars');
 
 // set up app and port
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(session({
+    secret: 'Its a secret to everybody',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}));
 
 // set up template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
